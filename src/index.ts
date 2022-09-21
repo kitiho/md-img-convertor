8726ebe7812a11908e15336f28338c80afc92744
enoshima7 <duanjipeng0328@gmail.com>
2022-09-21 12:14:42 +0800

    feat: 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
-export const one = 1
-export const two = 2
+import { copyFile } from 'fs/promises'
+import fs from 'fs'
+import { resolve } from 'path'
+import { marked } from 'marked'
+
+type MdImageNode = marked.Tokens.Image
+interface Config {
+  imgDist: string
+  folderLink?: string
+}
+export function convertor(source: string, config: Config) {
+  const imageNodes: MdImageNode[] = []
+
+  // walk all tokens to find all image node
+  const walkTokens = (token: marked.Token) => {
+    if (token.type === 'image')
+      imageNodes.push(token)
+  }
+
+  marked.use({ walkTokens })
+
+  marked.parse(source)
+  const folderLink = config?.folderLink
+  const imgDist = config.imgDist
+  let res = source
+  for (const item of imageNodes) {
+    // in case of space in path
+    const originFilePath = item.href.replace(' ', '%20')
+    const fileName = item.text
+    const extension = getExtension(item.href)
+    copy(originFilePath, imgDist, fileName, extension)
+    if (folderLink)
+      res = res.replace(item.href, `${folderLink}/${fileName}.${extension}`)
+  }
+  return res
+}
+function copy(originFilePath: string, dist: string, fileName: string, extension: string) {
+  try {
+    if (!fs.existsSync(dist))
+      fs.mkdirSync(dist)
+    copyFile(originFilePath, resolve(`${dist}/${fileName}.${extension}`)).catch((e) => {
+      console.warn(e)
+    })
+  }
+  catch (error) {
+    return error
+  }
+}
+function getExtension(fileName = '') {
+  return fileName.substring(fileName.lastIndexOf('.') + 1)
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,16 @@
+import { resolve } from 'path'
 import { describe, expect, it } from 'vitest'
-
+import { convertor } from '../src'
+const str = `
+![image-20220921105317781](/Users/duanjipeng/Desktop/leetcode/assets/image-20220921105317781.png)
+`
 describe('should', () => {
   it('exported', () => {
-    expect(1).toEqual(1)
+    const res = convertor(str, { imgDist: resolve(__dirname, 'assets'), folderLink: 'https://raw.githubusercontent.com/kitiho/leetcode/main/assets' })
+    expect(res).toMatchInlineSnapshot(`
+      "
+      ![image-20220921105317781](https://raw.githubusercontent.com/kitiho/leetcode/main/assets/image-20220921105317781.png)
+      "
+    `)
   })
 })
